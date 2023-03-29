ff1bd4f2e1c41a3c728e30888936fb0b642a17a4
MikhailSumarokov95 <108895364+MikhailSumarokov95@users.noreply.github.com>
2023-03-29 22:35:10 +0500

    0.32

--- Abc-Shooter/Assets/Menu/BattlePass/Scripts/BattlePassRewarder.cs
+++ Abc-Shooter/Assets/Menu/BattlePass/Scripts/BattlePassRewarder.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class BattlePassRewarder : MonoBehaviour
 {
-    public Action OnBoughtBattlePass;
-    [SerializeField] private RewardBattlePassPerLevel[] _rewardBattlePassPerLevel;
+    [SerializeField] private RewardBattlePass[] _rewardBattlePassPerLevel;
     [SerializeField] private LevelAchievementMark[] levelAchievementMark;
 
     private void OnEnable()
@@ -19,19 +18,14 @@ public class BattlePassRewarder : MonoBehaviour
     {
         Progress.SetBoughtBattlePass();
         for (var i = 1; i < FindObjectOfType<Level>().CurrentLevel + 1; i++)
-            Reward(_rewardBattlePassPerLevel[i].IsHaveBattlePassReward);
-
-        OnBoughtBattlePass?.Invoke();
+            Reward(_rewardBattlePassPerLevel[i]);
     }
 
     public void RewardPerLevel()
     {
         var currentLevel = FindObjectOfType<Level>().CurrentLevel;
-
-        Reward(_rewardBattlePassPerLevel[currentLevel].IsNotHaveBattlePassReward);
-
         if (Progress.IsBoughtBattlePass())
-            Reward(_rewardBattlePassPerLevel[currentLevel].IsHaveBattlePassReward);
+            Reward(_rewardBattlePassPerLevel[currentLevel]);
     }
 
     private void Reward(RewardBattlePass reward)
@@ -41,15 +35,13 @@ public class BattlePassRewarder : MonoBehaviour
             foreach (var weapon in reward.NameWeapon)
                 Progress.SetBuyWeapon(weapon);
         }
-
-        if (reward.AmountMoney != 0) 
+        if (reward.AmountMoney != 0)
             FindObjectOfType<Money>().MakeMoney(reward.AmountMoney);
     }
 
     private void EnableLevelAchievementMark()
     {
         var currentLevel = FindObjectOfType<Level>(true).CurrentLevel;
-
         for (var i = 1; i < levelAchievementMark.Length; i++)
         {
             levelAchievementMark[i].CloseImage.gameObject.SetActive(!(i <= currentLevel - 1));
@@ -58,17 +50,11 @@ public class BattlePassRewarder : MonoBehaviour
     }
 
     [Serializable]
-    private class RewardBattlePassPerLevel
-    {
-        public RewardBattlePass IsNotHaveBattlePassReward;
-        public RewardBattlePass IsHaveBattlePassReward;
-    }
-
-    [Serializable]
     private class RewardBattlePass
     {
         public WeaponBehaviour.Name[] NameWeapon;
         public int AmountMoney;
+        public int NumberSkinWeapons = -1;
     }
 
     [Serializable]
